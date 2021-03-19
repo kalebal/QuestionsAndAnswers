174bb376c9cceb96dc8bb30466f29b5f0baa3f74
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-19 12:09:22 -0400

    Minor cleanup

--- etl/answers.js
+++ etl/answers.js
@@ -24,15 +24,11 @@ let stream = fs.createReadStream(csvPath + 'answers.csv')
     } else {
       buffer[data.question_id] = [data];
     }
-    //when count hits 1000 (?)
-
+    //checking the lambda value -- when the chain is long, insert and empty that buffer
     if (buffer[data.question_id].length >= 10) {
       console.log(`inserting ${buffer[data.question_id].length} into`, data.question_id);
       let answers = buffer[data.question_id];
       return Answer.insertMany(answers)
-        // .then(() => {
-        //   return addAnswersToQuestion(data.question_id, answers);
-        // })
         .then(() => {
           count -= buffer[data.question_id].length;
           buffer[data.question_id] = [];
@@ -42,7 +38,7 @@ let stream = fs.createReadStream(csvPath + 'answers.csv')
           console.log(err);
         });
     } else {
-      //catch where there aren't 10 answers
+      //catches the leftovers that don't have 10 answers per question
       if (count >= 10000) {
         console.log('batch add');
         batchAdd(buffer).then(() => {
@@ -60,53 +56,15 @@ let stream = fs.createReadStream(csvPath + 'answers.csv')
     console.log('err', err);
   })
   .on('end', () => {
-    var promises = Object.entries(buffer).map(([id, answers]) => {
-      return Answer.insertMany(answers)
-        .then(() => {
-          addAnswersToQuestion(id, answers);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    });
-
-    Promise.all(promises)
-      .then((res) => {
-        console.log(batchNum, 'completed insert');
-        batchNum++;
-        count = 0;
-        buffer = {};
-        stream.resume();
-      })
-      .catch((err) => {
-        stream.destroy();
-        console.log('err updating', err);
-      });
+    //add all the leftovers still in the HT
+    batchadd(buffer);
   });
 
 
-//returns a promise
-let addAnswersToQuestion = (question_id, answers) => {
-  return Question.update(
-    { id: question_id },
-    { $push: { answers: answers } },
-    { writeConcern: { w: 0 } },
-    function (error, success) {
-      if (error) {
-        console.log(error);
-      } else {
-        // console.log(success);
-      }
-    }
-  );
-};
 
 let batchAdd = (buffer) => {
   var promises = Object.entries(buffer).map(([id, answers]) => {
     return Answer.insertMany(answers)
-      .then(() => {
-        addAnswersToQuestion(id, answers);
-      })
       .catch((err) => {
         console.log(err);
       });
