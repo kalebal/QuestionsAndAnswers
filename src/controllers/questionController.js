5dd8134d9d8e97a0333214be1fe6b81c3d8d04e1
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-20 11:05:48 -0400

    Implement getQuestion function

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,11 +1,21 @@
 var { Question } = require('../models/schemas');
+var db = require('../dataServices/questions');
 
 module.exports.question_list = function (req, res) {
   res.send('NOT IMPLEMENTED: question LIST');
 }
 
 module.exports.question_get = function (req, res) {
-  res.send(`NOT IMPLEMENTED: GET QUESTION FOR PRODUCT ${req.params.product_id}`);
+  db.getQuestions(req.params.product_id).then((result) => {
+    console.log('result found', result);
+    var response = {
+      product_id: req.params.product_id,
+      results: result
+    }
+    res.status(200).send(response);
+  }).catch((error) => {
+    res.status(200).send('error: ' + error);
+  });
 }
 
 module.exports.question_update_helpful = function (req, res) {
@@ -14,4 +24,8 @@ module.exports.question_update_helpful = function (req, res) {
 
 module.exports.question_update_report = function (req, res) {
   res.send('NOT IMPLEMENTED: question REPORT');
+}
+
+module.exports.add_question = function (req, res) {
+  res.send('NOT IMPLEMENTED: ADD QUESTION');
 }
