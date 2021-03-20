5770044e81b3c25bf1d9989df7329343018b68a7
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-20 11:04:26 -0400

    Separate database connection from server init

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const BodyParser = require('body-parser');
-const routes = require('./routes/index.js');
-const MongoClient = require("mongodb").MongoClient;
-var { DATABASE_NAME, CONNECTION_URL } = require('../config.js');
+const router = require('./routes/index.js');
+const dbConnection = require('./dataServices/dbUtil.js');
 const app = express();
 const port = 3000;
 
@@ -10,24 +9,20 @@ const port = 3000;
 
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
-app.use('/qa/', routes);
+app.use('/qa/', router);
 
 
 app.get('/', (req, res) => {
   res.send(`Hello World! -- you're not looking for questions, are you?`);
 });
 
+dbConnection.connect((err, client) => {
+  if (err) {
+    console.log(err);
+  }
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-  MongoClient.connect(CONNECTION_URL,
-    { useNewUrlParser: true },
-    (error, client) => {
-    if (error) {
-      throw error;
-    }
-    database = client.db(DATABASE_NAME);
-    collection = database.collection("personnel");
-    console.log("Connected to `" + DATABASE_NAME + "`!");
-  });
+
 });
 
