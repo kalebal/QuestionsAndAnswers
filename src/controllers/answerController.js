d39ddb17cecf2f81fba212b1e9fcee6d40deaf65
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-20 11:05:00 -0400

    Removed uneeded route

--- src/controllers/answerController.js
+++ src/controllers/answerController.js
@@ -1,11 +1,12 @@
-var { Answer } = require('../models/schemas');
-
-module.exports.answer_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: ANSWER LIST');
-}
+var db = require('../dataServices/answers');
 
 module.exports.answer_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: GET ANSWERS FOR QUESTION');
+  db.getAnswers(req.params.question_id).then((result) => {
+    console.log('result found', result);
+    res.status(200).send(result);
+  }).catch((err) => {
+    res.status(200).send('error');
+  });
 }
 
 module.exports.answer_update_helpful = function (req, res) {
@@ -14,5 +15,9 @@ module.exports.answer_update_helpful = function (req, res) {
 
 module.exports.answer_update_report = function (req, res) {
   res.send('NOT IMPLEMENTED: ANSWER REPORT');
+}
+
+module.exports.add_answer = function (req, res) {
+  res.send('NOT IMPLEMENTED: ADD ANSWER');
 }
 
