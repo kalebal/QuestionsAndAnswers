51d53c068408c094c8e39e7bf9f425a9cd3aca83
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-22 16:23:58 -0400

    remove console log

--- src/dataServices/readOps.js
+++ src/dataServices/readOps.js
@@ -2,7 +2,6 @@ const dbConnection = require('./dbUtil.js');
 
 module.exports.getList = (collection, query, fields, page, count) => {
   db = dbConnection.getDatabase();
-  console.log('fields', fields)
   return db.collection(collection).find(query)
   .project(fields)
   .skip((page - 1) * count)
