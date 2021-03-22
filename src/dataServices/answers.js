5dd3c6e1eab5f94c292f84d7109a55cf5f1484f0
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-22 12:17:44 -0400

    Created mark helpful and report queries using array filters

--- src/dataServices/answers.js
+++ src/dataServices/answers.js
@@ -1,7 +1,11 @@
 const readDb = require('./readOps.js');
+const writeDb = require('./writeOps.js');
 
 module.exports.getAnswers = (id, page = 1, count = 5) => {
-  let query = { question_id: parseInt(id) };
+  let query = {
+    question_id: parseInt(id),
+    reported: 0
+  };
   let omitFields = {
     'question_id': 0,
     'answerer_email': 0
@@ -10,21 +14,55 @@ module.exports.getAnswers = (id, page = 1, count = 5) => {
 };
 
 module.exports.markHelpful = (answer_id) => {
-  let filter = { answer_id: answer_id };
+  let filter = { _id : parseInt(answer_id) };
   let updateDoc = {
     $inc: {
-      helpful: 1
+      'helpful': 1
     }
   }
-  return writeDb.updateDoc('answers', filter, updateDoc)
+  //this updates within the answers collection and returns the answer doc
+  //which provides info needed to update the answer where it's embedded in a question doc
+  return writeDb.findAndUpdateDoc('answers', filter, {
+    $inc: {
+      'helpful': 1
+    }
+  })
+    .then((doc) => {
+      console.log('found answer', doc);
+      let update = { $inc: { "answers.$[elem].helpful": 1 } };
+      let arrayFilters = { arrayFilters: [{ "elem._id": parseInt(doc._id) }] };
+      return updateWithinQuestion(parseInt(doc.question_id), update, arrayFilters);
+    });
 };
 
 module.exports.report = (answer_id) => {
   let filter = { answer_id: answer_id };
   let updateDoc = {
     $inc: {
-      report: 1
+      'report': 1
     }
   }
-  return writeDb.updateDoc('answers', filter,)
+  //see comments on line 22
+  return writeDb.findAndUpdateDoc('answers', filter, updateDoc)
+    .then((doc) => {
+      let query = [{
+        _id: parseInt(doc.question_id),
+      },
+      { $inc: { "answers.$[elem].reported": 1 } },
+      { arrayFilters: [{ "elem._id": doc._id }] }];
+
+      updateWithinQuestion(query);
+  })
+
+};
+
+let updateWithinQuestion = (id) => {
+  let filter =  {
+    _id: id
+  }
+  console.log('update within question');
+  return writeDb.updateDoc('questions', filter, update, options).then((doc) => {
+    console.log('found q doc', doc);
+    return doc;
+  });
 };
