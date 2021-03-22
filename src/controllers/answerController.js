a292e1d764cfddc552d6301f8dd6befcb5cbcdcb
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-22 16:20:27 -0400

    Normalize function names

--- src/controllers/answerController.js
+++ src/controllers/answerController.js
@@ -1,24 +1,46 @@
 var db = require('../dataServices/answers');
 
 module.exports.answer_get = function (req, res) {
-  var { question_id, page, count } = req.params;
+  var { question_id } = req.params;
+  var { page, count } = req.query;
   db.getAnswers(question_id, page, count).then((result) => {
-    console.log('result found', result);
-    res.status(200).send(result);
+    console.log('result', result);
+    var response = {
+      question_id: result[0]._id,
+      page: req.params.page,
+      count: req.params.count,
+      results: result[0]['answers']
+    }
+    res.status(200).send(response);
   }).catch((err) => {
-    res.status(200).send('error');
+    res.status(200).send(err);
   });
 }
 
 module.exports.answer_update_helpful = function (req, res) {
-  res.send('NOT IMPLEMENTED: ANSWER MARK HELPFUL');
+  console.log('answer helpful');
+  var { answer_id } = req.params;
+  db.markHelpful(answer_id).then((result) => {
+    res.status(204).send();
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 }
 
 module.exports.answer_update_report = function (req, res) {
-  res.send('NOT IMPLEMENTED: ANSWER REPORT');
+  var { answer_id } = req.params;
+  db.report(answer_id).then((result) => {
+    res.status(204).send();
+  }).catch((err) => {
+    res.status(400).send('error');
+  });
 }
 
 module.exports.add_answer = function (req, res) {
-  res.send('NOT IMPLEMENTED: ADD ANSWER');
+  var { question_id } = req.params;
+  var newAnswer =  req.body;
+  db.addAnswer(question_id, newAnswer).then(() => {
+    res.status(201).send();
+  });
 }
 
--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -1,15 +1,11 @@
 var { Question } = require('../models/schemas');
 var db = require('../dataServices/questions');
 
-module.exports.question_list = function (req, res) {
-  res.send('NOT IMPLEMENTED: question LIST');
-}
-
 module.exports.question_get = function (req, res) {
-  db.getQuestions(req.params.product_id).then((result) => {
-    console.log('result found', result);
+  var { product_id, page, count } = req.query;
+  db.getQuestions(product_id, page, count).then((result) => {
     var response = {
-      product_id: req.params.product_id,
+      product_id: req.query.product_id,
       results: result
     }
     res.status(200).send(response);
@@ -27,5 +23,9 @@ module.exports.question_update_report = function (req, res) {
 }
 
 module.exports.add_question = function (req, res) {
-  res.send('NOT IMPLEMENTED: ADD QUESTION');
+  db.addQuestion(req.body).then(() => {
+    res.status(201).send();
+  });
 }
+
+
