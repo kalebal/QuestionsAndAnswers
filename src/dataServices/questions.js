52828f3bc39ad72694e0e27b1caa61289d36230c
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-22 16:06:43 -0400

    filter out reported questions

--- src/dataServices/questions.js
+++ src/dataServices/questions.js
@@ -2,12 +2,19 @@ const readDb = require('./readOps.js');
 const writeDb = require('./writeOps.js');
 
 module.exports.getQuestions = (id, page = 1, count = 5) => {
-  let query = { product_id: parseInt(id) };
+  let query = {
+    product_id: parseInt(id),
+    reported: 0
+  };
   let omitFields = {
     'product_id': 0,
     'asker_email': 0
   };
   return readDb.getList('questions', query, omitFields, page, count);
+};
+
+module.exports.addQuestion = () => {
+
 };
 
 module.exports.markHelpful = (question_id) => {
