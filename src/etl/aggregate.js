cb77f7c4d2a713a4555057eeba1ff942d882bc20
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-23 13:48:06 -0400

    Combine pipelines for npm script

--- src/etl/aggregate.js
+++ src/etl/aggregate.js
@@ -1,90 +1,50 @@
 
 var MongoClient = require('mongodb').MongoClient;
-var {DATABASE_NAME, CONNECTION_URL} = require('../config.js');
-
 
 /*
-This sets up the mongodb client so aggregates can run using node
+This sets up the mongodb client to aggregate using node
 */
-MongoClient.connect(CONNECTION_URL, function (err, db) {
+MongoClient.connect(process.env.CONNECTION_URL, function (err, db) {
   if (err) throw err;
-  var dbo = db.db(DATABASE_NAME);
-  //this adds each photo's url to its respective answer as an array
-
-  dbo.collection('photos').aggregate(
-      { $group:
-        {
-          _id: "$answer_id", photos: { $push: '$url'}
-          //ex: { _id: ~answer#123~ photos: [~url1~, ~url2~]}
-        }},
-      { $addFields:
-        {
-          id: "$_id" //duplicate id field to match answer index
-        } },
-      { $unset: '_id'
-      //_id matches the photo _id, which
-      //will prevent merging with answers
-      },
-      { $merge:
-        {
-          into: 'answers',
-          on: 'id',
-          //find where the photo group's answer_id matches an answer's indexed id
-          //this adds the photos property to the answer document
-        }
-      });
+  var dbo = db.db(process.env.DATABASE_NAME);
+  aggregatePhotos(db)
+    .then(() => {
+    aggegateAnswers(db);
+    })
+    .catch((err) => {
+    console.log(err);
+  });
 };
 
-//same step as above minus the comments
-//all the commands below are for use in mongo CLI
-db.photos.aggregate(
-    { $group:
+function aggregatePhotos (db) {
+  db.collection('Photos').aggregate([
+    {
+      $group:
       {
-        _id: "$answer_id", photos: { $push: '$url'}
-        //ex: { _id: ~answer#123~ photos: [~url1~, ~url2~]}
-      }},
-    { $addFields:
-      {
-        id: "$_id" //duplicate id field to match answer index
-      } },
-    { $unset: '_id'
-    //_id matches the photo _id, which
-    //will prevent merging with answers
-    },
-    { $merge:
-      {
-        into: 'answers',
-        on: 'id',
-        //find where the photo group's answer_id matches an answer's indexed id
-        //this adds the photos property to the answer document
+        _id: "$answer_id", photos: { $push: '$url' }
       }
-    })
+    },
+    {
+      $merge:
+      {
+        into: 'Answers',
+        on: '_id',
+      }
+    }], { "allowDiskUse": true });
+};
 
 //this embeds all answers as an array of subdocuments into their respective questions
-db.answers.aggregate([
-  {
-    $group:
-    {_id: "$question_id", answers: { $push: '$$ROOT' }}
-  },
-  {
-    $merge:
+function aggegateAnswers(db) {
+  db.collection('Answers').aggregate([
     {
-      into: 'questions',
-      on: '_id',
-    }
-  }], { "allowDiskUse": true });
-
-db.photos.aggregate([
-  {
-    $group:
+      $group:
+      {_id: "$question_id", answers: { $push: '$$ROOT' }}
+    },
     {
-      _id: "$answer_id", photos: { $push: '$url' }
-    }
-  },
-  {
-    $merge:
-    {
-      into: 'answers',
-      on: '_id',
-    }
-  }], { "allowDiskUse": true })
+      $merge:
+      {
+        into: 'Questions',
+        on: '_id',
+      }
+    }], { "allowDiskUse": true });
+}
