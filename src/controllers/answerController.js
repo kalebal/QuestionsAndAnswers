91aef3064b7a1f542a0472293acc9704a1af2681
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-23 12:25:36 -0400

    Removed console.log

--- src/controllers/answerController.js
+++ src/controllers/answerController.js
@@ -4,7 +4,6 @@ module.exports.answer_get = function (req, res) {
   var { question_id } = req.params;
   var { page, count } = req.query;
   db.getAnswers(question_id, page, count).then((result) => {
-    console.log('result', result);
     var response = {
       question_id: result[0]._id,
       page: req.params.page,
@@ -18,7 +17,6 @@ module.exports.answer_get = function (req, res) {
 }
 
 module.exports.answer_update_helpful = function (req, res) {
-  console.log('answer helpful');
   var { answer_id } = req.params;
   db.markHelpful(answer_id).then((result) => {
     res.status(204).send();
