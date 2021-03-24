8c69babe6bec3a4dc30ac1a86191567f76709de3
Kaleb Lyda <kaleblyda14@gmail.com>
2021-03-24 13:27:49 -0400

    set limit to default 5

--- src/dataServices/readOps.js
+++ src/dataServices/readOps.js
@@ -1,8 +1,9 @@
 const dbConnection = require('./dbUtil.js');
 
-module.exports.getList = (collection, query, fields, page, count) => {
+module.exports.getList = (collection, query, fields, page, count = 5) => {
   db = dbConnection.getDatabase();
   return db.collection(collection).find(query)
+  .limit(count)
   .project(fields)
   .skip((page - 1) * count)
   .limit(count).toArray();
